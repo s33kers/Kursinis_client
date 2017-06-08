14517cbdf94f5ab9a8ab1f3171e76fe093406a95
s33kers <tadas235@gmail.com>
2017-06-08 18:53:08 +0300

    deleted unused dependency

--- src/main/java/kursinis/client/exceptions/InputFormatException.java
+++ src/main/java/kursinis/client/exceptions/InputFormatException.java
@@ -1,22 +1,8 @@
 package kursinis.client.exceptions;
 
 public class InputFormatException extends Exception {
-    private int lineNumber = -1;
 
     public InputFormatException(String message) {
         super(message);
-    }
-
-    public InputFormatException(String message, int lineNr) {
-        super(message);
-        lineNumber = lineNr;
-    }
-
-    public void setLineNumber(int lineNr) {
-        lineNumber = lineNr;
-    }
-
-    public int getLineNr() {
-        return lineNumber;
     }
 }
